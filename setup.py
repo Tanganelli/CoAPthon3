357b1afe4d09e6abd79a153111e46a9918b7172c
giacomo.tanganelli@for.unipi.it <hitachicm500et>
2018-01-25 10:55:15 +0100

    Porting to Python3

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from distutils.core import setup
 
 setup(
     name='CoAPthon3',
-    version='1.0.0',
+    version='1.0.1',
     packages=['coapthon', 'coapthon.caching', 'coapthon.layers', 'coapthon.client', 'coapthon.server', 'coapthon.messages',
               'coapthon.forward_proxy', 'coapthon.resources', 'coapthon.reverse_proxy'],
     url='https://github.com/Tanganelli/CoAPthon3',
     license='MIT License',
     author='Giacomo Tanganelli',
     author_email='giacomo.tanganelli@for.unipi.it',
-    download_url='https://github.com/Tanganelli/CoAPthon3/archive/1.0.tar.gz',
+    download_url='https://github.com/Tanganelli/CoAPthon3/archive/1.0.1.tar.gz',
     description='CoAPthon is a python library to the CoAP protocol. ',
     scripts=['coapserver.py', 'coapclient.py', 'exampleresources.py', 'coapforwardproxy.py', 'coapreverseproxy.py'],
     requires=['sphinx', 'cachetools']
